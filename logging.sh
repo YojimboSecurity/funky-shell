6a7b2997f4597dcc896966ec731b47e083d1a666
David Johnson <david.johnson@binarydefense.com>
2022-11-05 12:33:25 -0400

    Add Logging Lib
    I added a logging library and a supporting color library. This logging
    library adds basic logging functions like info, debug, and err. The
    logging outputs a standard time stamp, the type of logging, and the
    message. The different logging functions such as info err are in
    distinct colors. Naturally err is red and info in blue.

---
+++ logging.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# This is a library of logging functions that are used throughout the scripts
+# in this project.
+#
+# I try to follow the google srcipt style guide.
+# https://google.github.io/styleguide/shellguide.html
+# I also use shellcheck to make sure that I am not using any bashisms.
+# https://github.com/koalaman/shellcheck
+
+. ./tools/lib/color.sh
+
+#######################################
+# Err handling and logging
+#
+# Arguments:
+#   $*: Error message
+#######################################
+log::err(){
+    d=$(date "+%Y.%m.%d-%H:%M:%S")
+    echo -e "$Red[$d ERROR]:$Color_Off $*" >&2
+}
+
+#######################################
+# Info logging
+#
+# Arguments:
+#   $*: Info message
+#######################################
+log::info(){
+    d=$(date "+%Y.%m.%d-%H:%M:%S")
+    echo -e "$Blue[$d INFO]:$Color_Off $*"
+}
+
+#######################################
+# Debug logging
+#
+# Arguments:
+#   $*: Debug message
+log::debug(){
+    if [[ "${DEBUG}" == "true" ]]; then
+        d=$(date "+%Y.%m.%d-%H:%M:%S")
+        echo -e "$Yellow[$d DEBUG]:$Color_Off $*"
+    fi
+}
